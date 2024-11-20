b6db1941d7a1d505ed733a4f09f44559a661f74b
Jhon Anthony Quiliche Calderon <anthonyquili.dev@gmail.com>
2024-11-20 10:56:22 +0100

    Ejercicios con CLASES y metodos

---
+++ src/etapa_II/MainRacional.java
@@ -0,0 +1,39 @@
+package etapa_II;
+
+public class MainRacional {
+	public static void main(String[] args) {
+		Racional r1 = new Racional();
+		System.out.println("Racional 1: " + r1);
+		
+
+		Racional r2 = new Racional (r1);
+		System.out.println("Racional 2: " + r2.toString());
+		//r2.factorial(4);
+		Racional r5 = new Racional();
+		r5.setNumerador(21);
+		r5.setDenominador(3);
+		
+		Racional r6 = new Racional();
+		r6.setNumerador(20);
+		r6.setDenominador(5);
+		
+		if (r5.equals(r6)) {
+			System.out.println("Son iguales");
+		} else {
+			System.out.println("Son distintos");
+		}
+		if (r5.compareTo(r6) == 1) {
+			System.out.println("r5 es mayor que r6");
+		} else {
+			System.out.println("r5 es menor que r6");
+		}
+	
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/etapa_II/Racional.java
@@ -0,0 +1,169 @@
+package etapa_II;
+
+import java.util.Objects;
+
+public class Racional  implements Comparable<Racional>{
+
+	private int numerador;	// Numerador
+	private int denominador;	// Denominador
+			
+	// Constructor
+	public Racional() {
+		this.numerador = 0;
+		this.denominador = 1;
+	}
+	
+	//Constructor de copia
+	public Racional(Racional r) {
+		this.numerador = r.numerador;
+		this.denominador = r.denominador;
+	}
+	
+	// Constructor con parámetros
+	public Racional(int numerador, int denominador) {
+		this.numerador = numerador;
+		this.denominador = denominador;
+	}
+	
+	
+	// Método para simplificar un racional
+	public void simplificar() {
+		int mcd = mcd(this.numerador, this.denominador);
+		this.numerador = this.numerador / mcd;
+		this.denominador = this.denominador / mcd;
+	}
+	// Método para calcular el máximo común divisor
+	private int mcd(int a, int b) {
+		int mcd = 1;
+		int min = Math.min(a, b);
+		for (int i = 1; i <= min; i++) {
+			if (a % i == 0 && b % i == 0) {
+				mcd = i;
+			}
+		}
+		return mcd;
+	}
+	
+	// Método para dividir dos racionales
+	public Racional dividir(Racional r) {
+		Racional res = new Racional();
+		res.numerador = this.numerador * r.denominador;
+		res.denominador = this.denominador * r.numerador;
+		return res;
+	}
+	
+	// Método para comparar dos racionales
+	public boolean equals(Racional r) {
+		return (this.numerador * r.denominador == r.numerador * this.denominador);
+	}
+	
+	//Metodo para restar un racional
+	public Racional restar() {
+		Racional res = new Racional();
+		res.numerador = -this.numerador;
+		res.denominador = this.denominador;
+		return res;
+	}
+	
+	// Método para sumar dos racionales
+	public Racional sumar(Racional r) {
+		Racional res = new Racional();
+		res.numerador = this.numerador * r.denominador + r.numerador * this.denominador;
+		res.denominador = this.denominador * r.denominador;
+		return res;
+	}
+	
+	// Método para restar dos racionales
+	public Racional restar(Racional r) {
+		Racional res = new Racional();
+		res.numerador = this.numerador * r.denominador - r.numerador * this.denominador;
+		res.denominador = this.denominador * r.denominador;
+		return res;
+	}
+	// Método para multiplicar dos racionales
+	public Racional multiplicar(Racional r) {
+		Racional res = new Racional();
+		res.numerador = this.numerador * r.numerador;
+		res.denominador = this.denominador * r.denominador;
+		return res;
+	}
+	// Método para calcular el factorial de un número
+	public int factorial(int numero) {
+        if (numero < 0) {
+            System.out.println("Error, el número debe ser no negativo.");
+        }
+        int resultado = 1;
+        for (int i = 1; i <= numero; i++) {
+            resultado *= i;
+        }
+        return resultado;
+    }
+	
+	//Metodo para comparar dos racionales
+	public boolean mayorQue(Racional r) {
+		return (this.numerador * r.denominador > r.numerador * this.denominador);
+	}
+	
+	
+	// Métodos para obtener el numerador y el denominador
+	public int getNumerador() {
+		return numerador;
+	}
+
+	public int getDenominador() {
+		return denominador;
+	}
+	
+	// Métodos para establecer el numerador y el denominador
+
+	public void setNumerador(int numerador) {
+		this.numerador = numerador;
+	}
+
+	public void setDenominador(int denominador) {
+        this.denominador = denominador;
+        }
+	
+	//Metodo para mostrar el racional
+	public String toString() {
+		return (numerador + "/" + denominador);
+	}
+
+	
+	public Double toDouble() {
+        return ((double) numerador / (double) denominador);
+        
+    }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(denominador, numerador);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Racional other = (Racional) obj;
+		
+		
+		return this.numerador * other.denominador == this.denominador * other.numerador;
+
+		//return denominador == other.denominador && numerador == other.numerador;
+	}
+
+	@Override
+	public int compareTo(Racional o) {
+		if (this.numerador * o.denominador > this.denominador * o.numerador) {
+			return 1;
+		}
+		return 0;
+
+	}
+	
+	
+}
